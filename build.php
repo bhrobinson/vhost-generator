1105dbd9b3e7bc565a2b0d88e9b9066f527e5697
Brandon Robinson <bhrobinson@me.com>
2012-12-28 08:02:18 -0600

    removed reliance on ezsql - only sqlite2.
    Built using sqlite3 pro commands.

--- build.php
+++ build.php
@@ -1,10 +1,4 @@
 <?php
-
-// Include ezSQL core
-include_once "ez_sql_core.php";
-// Include ezSQL database specific component
-include_once "ez_sql_sqlite.php";
-
 //Constants
  $vhost_command = "
 <VirtualHost *:80>
@@ -14,10 +8,11 @@ include_once "ez_sql_sqlite.php";
 </VirtualHost>
 ";
 function grab_config($config_var){
-	$db2 = new ezSQL_sqlite('./','vhosts.db');
-	$location_results = $db2->get_results("Select * from config where name='$config_var'");
-	$location_row = $db2->get_row();
-	return $location_row->command;
+	$db = new SQLite3('vhosts.db');
+	$location_results = $db->query("Select * from config where name='$config_var'");
+	$name=$location_results->fetcharray();
+	return $name[command];
+    $location_row->command;
 }
 
 function restart_apache(){
@@ -33,22 +28,23 @@ function restart_apache(){
 function generate_vhost_string($domain){
 	global $vhost_command;
 	$newstring = $vhost_command;
-	if ($domain->www_alias <>1){
+	if ($domain['www_alias']<>1){
 		$newstring = str_replace("ServerAlias www.{domain}", '', $newstring);
 	}
-	$newstring = str_replace("{domain}", $domain->domain, $newstring);
-	$newstring = str_replace("{forward}", $domain->forward, $newstring);
+	$newstring = str_replace("{domain}", $domain['domain'], $newstring);
+	$newstring = str_replace("{forward}", $domain['forward'], $newstring);
 	return $newstring;
 }
 
 function build_vhost($db) {
 	$file = grab_config('vhost_location');
-	$FileHandle = fopen($file, 'w') or die("can't open file");
+    $FileHandle = fopen($file, 'w') or die("can't open file");
 	echo "\n Generating the vhost file\n \n";
-	$results = $db->get_results("Select * from domain order by domain");
-	foreach ($results as $row) {
-		echo "writing "  . $row->domain . " to point to " . $row->forward . " \n";
-		 fwrite($FileHandle, generate_vhost_string($row));
+	$result=$db->query("SELECT * FROM domain");
+	$x=1;
+	while ($row = $result->fetchArray()) {
+		echo "writing "  . $row['domain'] . " to point to " . $row['forward'] . " \n";
+		fwrite($FileHandle, generate_vhost_string($row));
 	}
 	fclose($FileHandle);
 }
@@ -56,16 +52,20 @@ function build_vhost($db) {
 
 // Initialise database object and establish a connection
 // at the same time - db_path / db_name
-$db = new ezSQL_sqlite('./','vhosts.db');
+$db = new SQLite3('vhosts.db');
 //Verify db exists, and create table if it does not
-$results = $db->get_results("SELECT name FROM sqlite_master WHERE type='table' AND name like'domain%';");
-//$db->debug($results);
-if (!$results) {
+$results = $db->query("SELECT name FROM sqlite_master WHERE type='table';");
+
+if ((count($results->fetcharray()))<2) {
 	echo "Creating initial database";
-    $db->query("CREATE TABLE domain(Id integer PRIMARY KEY, domain text UNIQUE NOT NULL, forward  text UNIQUE NOT NULL, www_alias integer)");
+    $db->exec("CREATE TABLE domain(Id integer PRIMARY KEY, domain text UNIQUE NOT NULL, forward  text UNIQUE NOT NULL, www_alias integer)");
+	$db->exec("CREATE TABLE config(name text UNIQUE NOT NULL, command text UNIQUE NOT NULL)");
+	$db->exec('insert into config (name,command) values ("apache_location","/usr/sbin/apache")');
+	$db->exec('insert into config (name,command) values ("vhost_location","vhosts.conf")');
 }
 build_vhost($db);
-restart_apache();
+//restart_apache();
+
 ?>
 
 
